a992aa2b97a93a12c3664f83d8473001c000cab5
ibrahimdev23 <ibtheonemohamed13@gmail.com>
2024-08-21 22:50:58 +0700

    updates links on banner

--- habitappp/src/components/Banner.jsx
+++ habitappp/src/components/Banner.jsx
@@ -43,11 +43,11 @@ export const Banner = () => {
 
 <div  tabindex="-1" className="bg-gradient-to-r from-red-500 to-red-700 fixed z-50 flex flex-col md:flex-row justify-between w-[calc(100%-2rem)] p-4 -translate-x-1/2 bg-white rounded-lg shadow-sm lg:max-w-7xl left-1/2 top-6 dark:bg-gray-700 dark:border-gray-600">
     <div class="flex flex-col items-start mb-3 me-4 md:items-center md:flex-row md:mb-0">
-        <a href="https://flowbite.com/" class="flex items-center mb-2 border-gray-200 md:pe-4 md:me-4 md:border-e md:mb-0 dark:border-gray-600">
+        <h3 class="flex items-center mb-2 border-gray-200 md:pe-4 md:me-4 md:border-e md:mb-0 dark:border-gray-600">
             <span class="self-center text-lg font-semibold whitespace-nowrap dark:text-white">
             Consistency is key
             </span>
-        </a>
+        </h3>
         <p class="flex items-center text-sm font-normal text-zinc-50">
         <ol className='font-semibold  max-w-md space-y-1 list-decimal list-inside'>
           <li >
@@ -63,12 +63,12 @@ export const Banner = () => {
         </p>
     </div>
     <div class="flex items-center flex-shrink-0">
-        <a href="#" class="px-5 py-2 me-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        <a href="https://jamesclear.com/stop-procrastinating-seinfeld-strategy" class="px-5 py-2 me-2 text-xs font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
           Learn more
         </a>
         <button 
         onClick={() => setShowBanner(false)}
-        data-dismiss-target="#marketing-banner" type="button" class="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
+        data-dismiss-target="#marketing-banner" type="button" class="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-white-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
             </svg>
